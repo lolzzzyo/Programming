5a52530133468e1938d7e376792ffa0ec73a25d0
lolzzzyo <lolzzzyo@hotmail.com>
2018-09-20 10:43:09 +0200

    Initial commit

---
+++ pe4_1.py
@@ -0,0 +1,8 @@
+uurloon = float(input('Wat verdien je per uur: '))
+aantaluur = int(input('Hoeveel uur heb je gewerkt: '))
+salaris = uurloon * aantaluur
+
+tekst = str(aantaluur) + ' uur werken levert ' + str(salaris) + ' Euro op'
+print(tekst)
+
+
